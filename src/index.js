8a21b61912d2f851d4cc881ddd1335f9d622c3c4
teamlotusfs-sys <teamlotusfs@gmail.com>
2026-04-17 16:46:55 -0400

    Initialize React app in index.js

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
-// Contents of index.js
-console.log('NBA Rebuild Game');
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<React.StrictMode><App /></React.StrictMode>);
