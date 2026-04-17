bedb91e91e5c4378490e07afdd2136b1ca774829
teamlotusfs-sys <teamlotusfs@gmail.com>
2026-04-17 16:09:27 -0400

    Add index.js for the NBA Rebuild game

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// Entry point for the NBA Rebuild game
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
